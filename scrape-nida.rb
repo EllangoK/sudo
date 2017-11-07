240dd6441ddcb71a2caff55d24644808a138f08d
Michael Chary <mac389@gmail.com>
2017-11-07 08:10:18 -0500

    Create JSON record
    For each article extract link, title

--- scrape-nida.rb
+++ scrape-nida.rb
@@ -1,22 +1,31 @@
 require 'HTTParty'
 require 'Nokogiri'
-require 'Pry'
-require 'csv'
+require 'json'
+require 'ap'
 
 base_url = "http://ctndisseminationlibrary.org/protocols"
 no_of_studies = 60
 urls = Array.new(no_of_studies) { |i| "#{base_url}/ctn%04d.htm"%i }
 
+db = JSON.parse(File.read("./db.json"))
+
 urls.each do |url|
-	page = HTTParty.get(url)
-	parsed = Nokogiri::HTML(page)
+	page = Nokogiri::HTML(HTTParty.get(url))
 
-	x = parsed.css("table")
-	#If table contains the title "PRELIMINARY RESEARCH FINDINGS", follow the "get article link"
-
-	links = parsed.css("table tr td p a")
+	links = page.css("table tr td p a")
 	links.each do |link|
-		puts "bob"
-		puts link
+		if link.text == "get article"
+			link_to_article  = link["href"]
+			primary_research = Nokogiri::HTML(HTTParty.get(link_to_article))
+			
+			ap primary_research.css('p .text a')
+			#Fields to get, link to article
+			db << {"url" => link_to_article,
+					"title" => primary_research.css(".medheaderblue").text.split.join(' '),
+					"text" => "",
+					"keywords" =>""}
+		end
 	end
 end
+
+ap db 
